eb9333731ed93a7f70e2ebb27f766be3efcc2877
Ivan D <demin.ivan21@yandex.ru>
2025-03-26 21:40:46 +0300

    Added user and item controllers, service and in memory storage

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
 public class ItemRequestDto {
+
+    private Long id;
+    private String description;
+    private Long requestor;
+    private LocalDate created;
+
 }
--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,14 +1,15 @@
-package ru.practicum.shareit.user;
+package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+public class UserDto {
 
-    private Long id;
     private String name;
     @Email
     private String email;
